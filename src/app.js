d8fd0c584fda0f71c4ef72b2f915c920f67c01df
Sinem Karaaslan <sinemkrslnn@gmail.com>
2026-05-16 21:02:18 +0300

    feat: register all routes in app.js

--- src/app.js
+++ src/app.js
@@ -6,9 +6,19 @@ app.use(express.json());
 //Routeları bağla
 const authRoutes = require('./routes/authRoutes');
 const leagueRoutes = require('./routes/leagueRoutes');
+const raceRoutes = require('./routes/raceRoutes');
+const predictionRoutes = require('./routes/predictionRoutes');
+const predictionStandaloneRoutes = require('./routes/predictionStandaloneRoutes');
+const path = require('path');
+const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/leagues', leagueRoutes);
+app.use('/api/races', raceRoutes);
+app.use('/api/leagues/:leagueId/races/:raceId/predictions', predictionRoutes);
+app.use('/api/predictions', predictionStandaloneRoutes);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/api/users', userRoutes);
 
 //404
 app.use((req, res) => {
